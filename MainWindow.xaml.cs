06f2e5af9acf2362354d62ec74e8b29126757554
Alexandr Gaponenko <sag5223@gmail.com>
2023-06-07 21:43:44 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+namespace lift
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+
+
+
+    public partial class MainWindow : Window
+    {
+        private int currentfloor = 9;
+        private double speed = 0.5;
+        int brokenFloor;
+
+
+
+        public MainWindow()
+        {
+            Random rnd = new Random();
+            InitializeComponent();
+            brokenFloor = rnd.Next(1, 10);
+        }
+
+        private int UserControl1_floorchange(string flor)
+        {
+            int targetFloor = Int32.Parse(flor);
+            DoubleAnimation LiftAnim = new DoubleAnimation();
+            LiftAnim.From = Canvas.GetTop(image);
+            LiftAnim.By = (targetFloor - currentfloor) * 37 * (-1);
+            LiftAnim.Duration = TimeSpan.FromSeconds(Math.Abs(targetFloor - currentfloor) * speed);
+            image.BeginAnimation(TopProperty, LiftAnim);
+            currentfloor = targetFloor;
+
+            if (brokenFloor == targetFloor)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ UserControl1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+namespace lift
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public delegate int myHandler(string floor);
+        public event myHandler floorchange;
+       
+
+        public UserControl1()
+        {
+            InitializeComponent();
+            watchout.Visibility = Visibility.Hidden;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int isWarining;
+            isWarining = floorchange.Invoke((sender as Button).Content.ToString());
+            if (isWarining == 1) watchout.Visibility = Visibility.Visible;
+            else watchout.Visibility = Visibility.Hidden;
+        }
+    }
+
+}
